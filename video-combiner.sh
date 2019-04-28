c5b71daf3b5ad29df74e56653c20b1f532cfc797
tofran <franciscopcmarques@gmail.com>
2019-04-28 18:44:16 +0100

    code reorganization and general improvements

--- video-combiner.sh
+++ video-combiner.sh
@@ -6,16 +6,33 @@
 #   $ video-combiner.sh video1.avi video2.avi video3.avi
 
 MAX_SECONDS_BETWEEN_PARTS=5
-DATE_FORMAT="%Y-%m-%d_%H-%M"
 OUTPUT_FILE_EXTENSION=".avi"
+OUTPUT_DATE_FORMAT="%Y-%m-%d_%H-%M_%Z"
 
 set -e
+
+generate_video () {
+    local start_timstamp=$1; shift;
+    local files=( $@ )
+
+    if [ ${#files[@]} -ne 0 ]; then
+
+        output_file=$(date --date="@${start_timstamp}" +"${OUTPUT_DATE_FORMAT}")${OUTPUT_FILE_EXTENSION}
+        touch_date=$(date --date="@${start_timstamp}" +"${TOUCH_DATE_FORMAT}")
+
+        echo "${output_file} will be created from ${files[@]}"
+        read -p "Press enter to contune..."
+
+        mencoder -oac copy -ovc copy ${files[@]} -o ${output_file}
+
+    fi
+}
 
 files=( $@ )
 end_timestamps=( $(stat -c "%Y" ${files[@]}) )
 
 previous_end_timestamp=0
-current_files=()
+current_video_files=()
 current_output=""
 
 for i in "${!files[@]}"; do
@@ -27,24 +44,20 @@ for i in "${!files[@]}"; do
     start_timstamp=$(( ${end_timestamp} - ${lenght} ))
     interval=$(( ${start_timstamp} - ${previous_end_timestamp} ))
 
-    echo "-> ${file} (timestamp: ${start_timstamp}; lenght: ${lenght}; interval: ${interval})"
+    # echo "-> ${file} (timestamp: ${start_timstamp}; lenght: ${lenght}; interval: ${interval})"
 
     if [ "${interval}" -gt "${MAX_SECONDS_BETWEEN_PARTS}" ]; then
 
-        if [ ${#current_files[@]} -ne 0 ]; then
-            echo "${current_output} will be created from ${current_files[@]}"
-            read -p "Press enter to contune...."
+        generate_video "${current_video_start_timstamp}" "${current_video_files[@]}" 
 
-            mencoder -oac copy -ovc copy ${current_files[@]} -o ${current_output}
-        fi
-
-        current_output=$(date --date="@${start_timstamp}" --utc +"${DATE_FORMAT}")${OUTPUT_FILE_EXTENSION}
-        current_files=($file)
+        current_video_start_timstamp=${start_timstamp}
+        current_video_files=($file)
 
     else
-        current_files+=($file)
+        current_video_files+=($file)
     fi
 
     previous_end_timestamp=${end_timestamp}
 done
 
+generate_video "${current_video_start_timstamp}" "${current_video_files[@]}" 
