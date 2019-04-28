ae538aa643ba861677f150a7f40bb8ab3b3115f9
tofran <franciscopcmarques@gmail.com>
2019-04-28 17:29:06 +0100

    Modified date is the end date Fixed bug where the last clip was not grouped

--- video-combiner.sh
+++ video-combiner.sh
@@ -5,31 +5,32 @@
 # usage:
 #   $ video-combiner.sh video1.avi video2.avi video3.avi
 
-set -e
-
 MAX_SECONDS_BETWEEN_PARTS=5
 DATE_FORMAT="%Y-%m-%d_%H-%M"
 OUTPUT_FILE_EXTENSION=".avi"
 
+set -e
+
 files=( $@ )
-start_timestamps=( $(stat -c "%Y" ${files[@]}) )
+end_timestamps=( $(stat -c "%Y" ${files[@]}) )
 
-start_timstamp=99999999999
-end_timstamp=0
-
+previous_end_timestamp=0
 current_files=()
 current_output=""
 
 for i in "${!files[@]}"; do
     file=${files[i]}
-    start_timstamp=${start_timestamps[i]}
+    end_timestamp=${end_timestamps[i]}
 
     lenght=$(ffprobe -loglevel panic -show_format ${file} | sed -n '/duration/s/.*=//p' | sed "s/\..*//")
-    interval=$(( ${start_timstamp} - ${end_timstamp} ))
 
-    # echo "-> ${file} (timestamp: ${start_timstamp}; lenght: ${lenght}; interval: ${interval})"
+    start_timstamp=$(( ${end_timestamp} - ${lenght} ))
+    interval=$(( ${start_timstamp} - ${previous_end_timestamp} ))
+
+    echo "-> ${file} (timestamp: ${start_timstamp}; lenght: ${lenght}; interval: ${interval})"
 
     if [ "${interval}" -gt "${MAX_SECONDS_BETWEEN_PARTS}" ]; then
+
         if [ ${#current_files[@]} -ne 0 ]; then
             echo "${current_output} will be created from ${current_files[@]}"
             read -p "Press enter to contune...."
@@ -44,6 +45,6 @@ for i in "${!files[@]}"; do
         current_files+=($file)
     fi
 
-    end_timstamp=$(( ${start_timstamp} + ${lenght} ))
+    previous_end_timestamp=${end_timestamp}
 done
 
